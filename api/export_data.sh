2584b702e16084acee835a8558f056b4d66c827b
Shubham <ssakhuja@aramse.io>
2020-04-20 23:37:56 -0400

    fix loader

--- api/export_data.sh
+++ api/export_data.sh
@@ -4,7 +4,7 @@ suffix=$1
 PSQL="psql postgresql://postgres:postgres@db:5432"
 
 data_dir=data$suffix
-mkdir -p 
+mkdir -p $data_dir 
 for table in $($PSQL -t -c "SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';"); do
   db_table=${table}${suffix}
   echo "exporting table $db_table to $data_dir/$table.csv"
