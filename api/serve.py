d471fdc67ebcc59f8519ea11f9e17d4b3fb8342c
Shubham <ssakhuja@aramse.io>
2020-04-23 15:51:55 -0400

    fixg

--- api/serve.py
+++ api/serve.py
@@ -53,7 +53,7 @@ class covid:
   def GET(self):
     params = web.input(refresh=False, suffix='')
     global CACHE, LAST_CACHE_UPDATE, CACHE_VALIDITY
-    cache_expired = (datetime.utcnow() - LAST_CACHE_UPDATE).seconds*60 >= CACHE_VALIDITY
+    cache_expired = (datetime.utcnow() - LAST_CACHE_UPDATE).seconds*60 >= CACHE_VALIDITY if LAST_CACHE_UPDATE else False
     if params.refresh or not CACHE or cache_expired:
       print 'last updated: ' + str(LAST_CACHE_UPDATE) + ', ' + str((datetime.utcnow() - LAST_CACHE_UPDATE).seconds*60) + ' min ago'
       if 0 != subprocess.call(os.getcwd() + '/export_data.sh ' + params.suffix, shell=True):
